1a819f2b3a6ff2886958bb601add33091a028426
Jonathan Oliveira <jojortd0@gmail.com>
2020-09-19 16:44:15 -0300

    Commit inicial

---
+++ LogicaDeProgramacao/Logica.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace LogicaDeProgramacao
+{
+    public static class Logica
+    {
+        public static int BuscarSeculoPorAno(int ano)
+        {
+            var seculo = 0;
+
+            while (ano>=1)
+            {
+                seculo++;
+                ano -= 100;
+            }
+
+            return seculo;
+        }
+        public static List<string> GeradorDeCartelas()
+        {
+            Random random = new Random();
+            List<string> validaor = new List<string>();
+            int numero = 0, repetir = 0;
+            string vl1 = "", vl2 = "", vl3 = "", vl4 = "", vl5 = "", vl6 = "";
+
+            while (repetir < 20)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    numero = random.Next(1, 61);
+
+                    if (string.IsNullOrEmpty(vl1)) { vl1 = numero.ToString(); }
+                    else if (string.IsNullOrEmpty(vl2)) { vl2 = numero.ToString(); }
+                    else if (string.IsNullOrEmpty(vl3)) { vl3 = numero.ToString(); }
+                    else if (string.IsNullOrEmpty(vl4)) { vl4 = numero.ToString(); }
+                    else if (string.IsNullOrEmpty(vl5)) { vl5 = numero.ToString(); }
+                    else if (string.IsNullOrEmpty(vl6)) { vl6 = numero.ToString(); }
+
+                }
+                validaor.Add($"Cartela {repetir + 1}: {vl1}-{vl2}-{vl3}-{vl4}-{vl5}-{vl6}  ||");
+                vl1 = ""; vl2 = ""; vl3 = ""; vl4 = ""; vl5 = ""; vl6 = "";
+
+                repetir++;
+            }
+            return validaor;
+        }
+    }
+}
---
+++ LogicaDeProgramacao/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogicaDeProgramacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region BuscarSeculoPorAno
+            //Dado um ano inicial como parâmetro, a logica retorna o seculo conrrespondente ao ano informado
+            //Console.WriteLine("Informe um ano!");
+            //var ano = Convert.ToInt16(Console.ReadLine());
+            //var seculo = Logica.BuscarSeculoPorAno(ano);
+            //Console.WriteLine($"O Século conrrespondente é:{seculo}");
+            #endregion
+
+            #region GeradorDeCartelas
+            //Logica para gerar cartelas de sorteio
+            //List<string> numerosCriados = new List<string>();
+
+            //Console.WriteLine("================= Cartelas de sorteio ================\n");
+
+            //int i = 0;
+
+            //numerosCriados = Logica.GeradorDeCartelas();
+            //foreach (var numero in numerosCriados)
+            //{
+            //    Console.Write("{0,8}", numero);
+            //    if (i % 4 == 0) Console.WriteLine();
+            //    i++;
+            //}
+            #endregion
+
+            #region BuscaMenorNumeroDeNotas
+            //Dado um valor, a logica informa o menor numero de notas posiveis 
+            //Console.WriteLine("Entre com o valor");
+            //int entrada = Convert.ToInt16(Console.ReadLine());
+            //int cem = 0, cinquenta = 0, vinte = 0, dez = 0, cinco = 0, dois = 0, um = 0;
+            //int menorVl = entrada;
+            //while (menorVl > 0)
+            //{
+            //    if (menorVl >= 100) { menorVl -= 100; cem++; }
+            //    else if (menorVl >= 50) { menorVl -= 50; cinquenta++; }
+            //    else if (menorVl >= 20) { menorVl -= 20; vinte++; }
+            //    else if (menorVl >= 10) { menorVl -= 10; dez++; }
+            //    else if (menorVl >= 5) { menorVl -= 5; cinco++; }
+            //    else if (menorVl >= 2) { menorVl -= 2; dois++; }
+            //    else if (menorVl.Equals(1)) { menorVl -= 1; um++; }
+            //    continue;
+            //}
+
+            //Console.WriteLine("Deve Retornar:");
+            //Console.WriteLine($" {cem} células de r$100,00");
+            //Console.WriteLine($" {cinquenta} células de r$50,00");
+            //Console.WriteLine($" {vinte} células de r$20,00");
+            //Console.WriteLine($" {dez} células de r$10,00");
+            //Console.WriteLine($" {cinco} células de r$5,00");
+            //Console.WriteLine($" {dois} células de r$2,00");
+            //Console.WriteLine($" {um} células de r$1,00");
+            //Console.WriteLine($"Total:{entrada}");
+            #endregion
+        }
+    }
+}
